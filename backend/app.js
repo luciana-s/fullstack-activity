47a0a37ebea426c9f37342aa16071d9109b559bd
luciana-s <l.scalfaro@easy4pro.com>
2022-06-14 19:16:24 +0200

    post

--- backend/app.js
+++ backend/app.js
@@ -32,5 +32,41 @@ app.use((req, res, next) => {
   next();
 });
 
+//* get all stuff
+app.use("/api/products", (req, res, next) => {
+  Product.find()
+    .then((product) => {
+      res.status(200).json({ products: product });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+});
+
+// * save new objs from POST into db
+app.post("/api/products", (req, res, next) => {
+  const product = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId,
+  });
+  product
+    .save()
+    .then(() => {
+      res.status(201).json({
+        product
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+});
+
 // very important
 module.exports = app;
